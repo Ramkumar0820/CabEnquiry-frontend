e07945151220d00863436e66f2b28b3701c7ca65
Shobana Sundaram <shobanasundaram@Apples-iMac.local>
2026-04-15 11:40:10 +0530

    added sitemap

---
+++ src/app/sitemap.jsx
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://maduraisrmtravels.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://maduraisrmtravels.vercel.app/travels-in-madurai",
+      lastModified: new Date(),
+    },
+  ];
+}
