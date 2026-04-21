4666142bd4e5826f83efa0acade3b763fbd85813
Shobana Sundaram <shobanasundaram@Apples-iMac.local>
2026-04-21 17:51:08 +0530

    update sitemap

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,9 +7,11 @@ import Footer from "@/components/template/footer"
 const inter = Poppins({ subsets: ["latin"], weight: ['100', '300', '700'], variable: '--poppins-font' });
 
 export const metadata = {
-  title: "Madurai SRM Tours & Travels | Cab Booking, Airport Pickup & Outstation Trips",
-  description:
-    "Madurai SRM Tours & Travels offers reliable cab booking, airport pickup and drop, rental and outstation travel services across Madurai and Tamil Nadu. Book safe, comfortable, and affordable rides with experienced drivers.",
+  title: "Best Taxi & Travels in Madurai | Madurai SRM Tours & Travels",
+  description: "Looking for reliable taxi services in Madurai? SRM Tours & Travels provides Madurai airport pickups, outstation cabs to Rameshwaram, Kodaikanal, and daily rental packages. Book your safe ride today!",
+  // title: "Madurai SRM Tours & Travels | Cab Booking, Airport Pickup & Outstation Trips",
+  // description:
+  //   "Madurai SRM Tours & Travels offers reliable cab booking, airport pickup and drop, rental and outstation travel services across Madurai and Tamil Nadu. Book safe, comfortable, and affordable rides with experienced drivers.",
   verification: {
     google: "iBHkP0laoxJUkWDxDeHBoqt6-5GK0SU7pksLZBaa1nk",
   },
